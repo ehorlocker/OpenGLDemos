7f637c40426b22d56952614bd19315a1f57f6e23
ehorlocker <ehorlocker@gmail.com>
2024-05-20 22:23:10 -0400

    made a window :)

--- OpenGLDemos/OpenGLDemos.cpp
+++ OpenGLDemos/OpenGLDemos.cpp
@@ -1,14 +1,56 @@
 ﻿#include "OpenGLDemos.h"
-#include "imgui.h"
 #include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	Core core;
-	core.foo();
+	GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1920, 1080, "OpenGLDemos", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    /* Call after making valid OpenGL context */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    {  
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+         
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+    
+    }
+    glfwTerminate();
+
+    return 0;
 
 	return 0;
 }
